4caec1a6401cdac3e505a490c374dbab24051645
Nicy Malanda <n.malanda@be-tse.it>
2023-12-25 09:23:55 +0100

    setup

--- src/main/java/com/mainthreadlab/weinv/config/WebSecurityConfiguration.java
+++ src/main/java/com/mainthreadlab/weinv/config/WebSecurityConfiguration.java
@@ -16,9 +16,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
